fd0bd1a2ac1717fab1e5eaf5917284514413b85d
marekdrab <marek.drablp@gmail.com>
2023-10-25 20:35:54 +0200

    cv 4.1.5

--- app/src/main/java/com/example/zadanie/FeedFragment.kt
+++ app/src/main/java/com/example/zadanie/FeedFragment.kt
@@ -1,12 +1,27 @@
 package com.example.zadanie
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<BottomBar>(R.id.bottom_bar).setActive(BottomBar.FEED)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.feed_recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        val feedAdapter = FeedAdapter()
+        recyclerView.adapter = feedAdapter
+        feedAdapter.updateItems(
+            listOf(
+                MyItem(R.drawable.baseline_feed, "Prvy"),
+                MyItem(R.drawable.baseline_map, "Druhy"),
+                MyItem(R.drawable.baseline_profile, "Treti"),
+            )
+        )
     }
 }
