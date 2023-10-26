8473541ccecc436e92a0551b1ced93236a3004f0
marekdrab <marek.drablp@gmail.com>
2023-10-26 10:22:21 +0200

    cv 4.2.1, 4.2.3

--- app/src/main/java/com/example/zadanie/FeedFragment.kt
+++ app/src/main/java/com/example/zadanie/FeedFragment.kt
@@ -1,27 +1,43 @@
 package com.example.zadanie
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
+    private lateinit var viewModel: FeedViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<BottomBar>(R.id.bottom_bar).setActive(BottomBar.FEED)
 
+        // Inicializácia ViewModel
+        viewModel = ViewModelProvider(this)[FeedViewModel::class.java]
+
         val recyclerView = view.findViewById<RecyclerView>(R.id.feed_recyclerview)
         recyclerView.layoutManager = LinearLayoutManager(context)
         val feedAdapter = FeedAdapter()
         recyclerView.adapter = feedAdapter
-        feedAdapter.updateItems(
+
+        // Pozorovanie zmeny hodnoty
+        viewModel.feed_items.observe(viewLifecycleOwner) { items ->
+            // Tu môžete aktualizovať UI podľa hodnoty stringValue
+            feedAdapter.updateItems(items)
+        }
+
+        viewModel.updateItems(
             listOf(
                 MyItem(0, R.drawable.baseline_feed, "Prvy"),
                 MyItem(1, R.drawable.baseline_map, "Druhy"),
                 MyItem(2, R.drawable.baseline_profile, "Treti"),
             )
         )
+
     }
 }
