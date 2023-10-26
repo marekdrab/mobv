d9b12d26f3b5edce5572f936e5a8c87dc1182805
marekdrab <marek.drablp@gmail.com>
2023-10-26 10:32:38 +0200

    cv 4.2.4

--- app/src/main/java/com/example/zadanie/ProfileFragment.kt
+++ app/src/main/java/com/example/zadanie/ProfileFragment.kt
@@ -5,15 +5,24 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
+    private lateinit var viewModel: FeedViewModel
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         view.findViewById<Button>(R.id.forgotten_pw).apply {
             setOnClickListener {
                 it.findNavController().navigate(R.id.action_change_pw)
             }
+        }
+        viewModel = ViewModelProvider(requireActivity())[FeedViewModel::class.java]
+
+        viewModel.feed_items.observe(viewLifecycleOwner) { items ->
+            // Tu môžete aktualizovať UI podľa hodnoty stringValue
+            Log.d("Profile", "prvky su $items")
         }
     }
 }
