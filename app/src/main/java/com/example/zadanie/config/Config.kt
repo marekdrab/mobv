4574380806ba75df81c26be9d0419c600a4a7a12
marekdrab <marek.drablp@gmail.com>
2023-11-30 08:14:51 +0100

    photos added

--- app/src/main/java/com/example/zadanie/config/Config.kt
+++ app/src/main/java/com/example/zadanie/config/Config.kt
@@ -3,4 +3,5 @@ package com.example.zadanie.config
 object Config {
     const val API_KEY = "c95332ee022df8c953ce470261efc695ecf3e784"
     const val SharedPreferences_KEY = "com.example.zadanie"
+    const val IMAGE_BASE_URL = "https://upload.mcomputing.eu/"
 }
