987f25aada84026ca12719cf617772587dfe9496
marekdrab <marek.drablp@gmail.com>
2023-12-04 19:46:53 +0100

    design updates & fixes

--- app/src/main/java/com/example/zadanie/widgets/BottomBar.kt
+++ app/src/main/java/com/example/zadanie/widgets/BottomBar.kt
@@ -1,10 +1,12 @@
 package com.example.zadanie.widgets
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import com.example.zadanie.R
 
@@ -21,6 +23,35 @@ class BottomBar : ConstraintLayout {
 
     fun setActive(index: Int) {
         active = index
+        updateItemAppearance()
+    }
+
+    private fun updateItemAppearance() {
+        val mapImage = findViewById<ImageView>(R.id.map)
+        val feedImage = findViewById<ImageView>(R.id.feed)
+        val profileImage = findViewById<ImageView>(R.id.profile)
+
+        // Reset all to inactive state
+        resetToInactive(mapImage, R.drawable.baseline_map)
+        resetToInactive(feedImage, R.drawable.baseline_feed)
+        resetToInactive(profileImage, R.drawable.baseline_profile)
+
+        // Set active state
+        when (active) {
+            MAP -> setActiveState(mapImage, R.drawable.baseline_map_white)
+            FEED -> setActiveState(feedImage, R.drawable.baseline_feed_white)
+            PROFILE -> setActiveState(profileImage, R.drawable.baseline_profile_white)
+        }
+    }
+
+    private fun resetToInactive(imageView: ImageView, drawable: Int) {
+        imageView.setImageResource(drawable)
+        imageView.background = ContextCompat.getDrawable(context, R.drawable.rounded_bg_transparent)
+    }
+
+    private fun setActiveState(imageView: ImageView, drawable: Int) {
+        imageView.setImageResource(drawable)
+        imageView.background = ContextCompat.getDrawable(context, R.drawable.rounded_bg) // replace with your active background
     }
 
     fun init() {
@@ -28,6 +59,14 @@ class BottomBar : ConstraintLayout {
             LayoutInflater.from(context)
                 .inflate(R.layout.widget_bottom_bar, this, false)
         addView(layout)
+        val mapImage = findViewById<ImageView>(R.id.map)
+        val feedImage = findViewById<ImageView>(R.id.feed)
+        val profileImage = findViewById<ImageView>(R.id.profile)
+
+        // Reset all to inactive state
+        resetToInactive(mapImage, R.drawable.baseline_map)
+        resetToInactive(feedImage, R.drawable.baseline_feed)
+        resetToInactive(profileImage, R.drawable.baseline_profile)
 
         layout.findViewById<ImageView>(R.id.map).setOnClickListener {
             if (active != MAP) {
